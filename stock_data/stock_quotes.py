ce4bdc4b13ab526cac6c346a3b7b5ca5699a5128
yusheng <zhouyusheng2016@126.com>
2021-04-09 11:43:04 +0800

    增加数据库插入及查询操作

--- stock_data/stock_quotes.py
+++ stock_data/stock_quotes.py
@@ -1,5 +1,6 @@
 from data_api.tushare import api as tsapi
 import pandas as pd
+from mydb.pool import DBPool
 
 
 def get_one_stock_quote_all(code):
@@ -32,3 +33,53 @@ def get_one_day_quotes(td):
     """
     quotes = tsapi.daily(trade_date=td)
     return quotes.rename({'ts_code': 'code'}, axis=1)
+
+
+def get_db_table_col():
+    """
+    获取股票数据表col
+    :return:
+    """
+    sql = "SHOW COLUMNS FROM research.stock_daily_quotes; "
+    try:
+        conn = DBPool.get_connection()
+        cur = conn.cursor()
+        cur.execute(sql)
+        cb = cur.fetchall()
+    except Exception as e:
+        raise e
+    finally:
+        cur.close()
+        conn.close()
+    return [t[0] for t in cb]
+
+
+def db_insert_datas(datas):
+    """
+    db插入stok quotes
+    :param datas:
+    :return:
+    """
+    db_col = ','.join(get_db_table_col())
+    sql = "INSERT INTO research.stock_daily_quotes (" + db_col +") " \
+          "VALUES(%s, STR_TO_DATE(%s, '%Y%m%d'), %s, %s, %s, %s, %s, %s, %s, %s, %s) " \
+          "ON DUPLICATE KEY UPDATE code = code; "
+
+    try:
+        print('insert in to stock_quotes')
+        conn = DBPool.get_connection()
+        cur = conn.cursor()
+        cur.executemany(sql, datas)
+        conn.commit()
+        print('insertion done')
+    except Exception as e:
+        raise e
+    finally:
+        cur.close()
+        conn.close()
+
+
+def db_insert_stock_quotes():
+    pass
+
+
